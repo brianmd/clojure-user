23743e2604ec34e3889a888050aee0c59ed47cb1
Brian Murphy-Dye <brian@murphydye.com>
2016-08-13 02:33:36 +0000

    Add latest & 1.0.0 tags.

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t bach/clojure-user . && docker push bach/clojure-user
+docker build -t bach/clojure-user:latest -t bach/clojure-user:1.0.0 . && docker push bach/clojure-user
